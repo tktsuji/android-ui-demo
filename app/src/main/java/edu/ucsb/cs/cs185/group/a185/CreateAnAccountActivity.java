544581e3816702f497ee6feae3f125b2fc6e29cd
Tricia Tsujimoto <tktsujimoto@gmail.com>
2017-03-05 16:33:03 -0800

    created create account and login screen

---
+++ app/src/main/java/edu/ucsb/cs/cs185/group/a185/CreateAnAccountActivity.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs.cs185.group.a185;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateAnAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_an_account);
+    }
+}
