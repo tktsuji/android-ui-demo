a01810b2651dd9b3039f4b2655bf84587ce845c1
Calvin <baseballercalvin@gmail.com>
2017-03-11 16:48:54 -0800

    Added bigly amounts of big posts

---
+++ app/src/main/java/edu/ucsb/cs/cs185/group/a185/EditProfileActivity.java
@@ -0,0 +1,22 @@
+package edu.ucsb.cs.cs185.group.a185;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/edu/ucsb/cs/cs185/group/a185/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/group/a185/MainActivity.java
@@ -2,7 +2,6 @@ package edu.ucsb.cs.cs185.group.a185;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -24,8 +23,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
