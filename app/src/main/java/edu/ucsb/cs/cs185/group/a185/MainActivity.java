f88fea18ad09ba677a4f3efba708bf7dd0efcc23
Andrew <andrewluo@umail.ucsb.edu>
2017-03-21 00:01:57 -0700

    Search function finally works

--- app/src/main/java/edu/ucsb/cs/cs185/group/a185/MainActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs185/group/a185/MainActivity.java
@@ -2,8 +2,14 @@ package edu.ucsb.cs.cs185.group.a185;
 
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
+import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,8 +19,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener, SearchView.OnQueryTextListener {
     static User user = User.getInstance();
     private PostManager postManager;
 
@@ -57,7 +62,62 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setOnQueryTextListener(this);
+        MenuItem searchItem = menu.findItem(R.id.search);
+        MenuItemCompat.setOnActionExpandListener(searchItem, new MenuItemCompat.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                FragmentManager manager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = manager.beginTransaction();
+                ListFragment fragment = new ListFragment();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+                manager.executePendingTransactions();
+                return true;
+            }
+        });
         return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        FilteredListFragment fragment = new FilteredListFragment();
+        Bundle bundle = new Bundle(2);
+        bundle.putInt("filterType", 1);
+        bundle.putString("filter", query);
+        fragment.setArguments(bundle);
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
+        manager.executePendingTransactions();
+        return true;
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        return false;
+    }
+
+    private void handleIntent(Intent intent){
+        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
+
+        }
     }
 
     @Override
@@ -94,6 +154,10 @@ public class MainActivity extends AppCompatActivity
             FragmentManager manager = getFragmentManager();
             FragmentTransaction fragmentTransaction = manager.beginTransaction();
             FilteredListFragment fragment = new FilteredListFragment();
+            Bundle bundle = new Bundle(2);
+            bundle.putInt("filterType", 2);
+            bundle.putString("filter", user.getUsername());
+            fragment.setArguments(bundle);
             fragmentTransaction.replace(R.id.fragment_container, fragment);
             fragmentTransaction.commit();
             manager.executePendingTransactions();
@@ -102,6 +166,13 @@ public class MainActivity extends AppCompatActivity
             Intent intent = new Intent(this, LoginActivity.class);
             user.purgeUser();
             startActivity(intent);
+        } else if(id == R.id.my_feed){
+            FragmentManager manager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = manager.beginTransaction();
+            ListFragment fragment = new ListFragment();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.commit();
+            manager.executePendingTransactions();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
